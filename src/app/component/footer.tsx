b6ab9d530cbcf38eb40be44571d0a402834ce684
Hooriya M. Fareed <parih3080@gmail.com>
2024-07-05 01:42:08 +0500

    push changes

--- src/app/component/footer.tsx
+++ src/app/component/footer.tsx
@@ -6,49 +6,50 @@ import { ImLinkedin } from "react-icons/im";
 
 const Footer = () => {
   return (
-    <div className="bg-blue-50 font-serif">
-      <footer className="text-gray-600 body-font">
-        <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-          <a className=" flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-            <Image
-              src={require("../../../public/assests/pictures/portfolio.png")}
-              width={180}
-              height={180}
-              alt="My Portfolio"
-              className="w-[300px] rounded-3xl h-[110px]"
-            />
-            <span className="ml-7 font-bold font-serif text-2xl">
-              MY
-              <span className=" font-bold ml-2 font-serif text-2xl text-blue-700">
-                PORTFOLIO
-              </span>
+    <footer className="text-gray-600 body-font">
+      <div className="bg-blue-50 font-serif">
+    <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+      <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+      <Image
+            src={require("../../../public/assests/pictures/portfolio.png")}
+            width={180}
+            height={180}
+            alt="My Portfolio"
+            className="w-[300px] rounded-3xl h-[110px]"
+          />
+        <span className="ml-3  font-bold font-serif text-xl">
+          MY
+            <span className=" font-bold ml-2 font-serif text-xl text-blue-700">
+              PORTFOLIO
             </span>
-          </a>
-          <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 font-serif sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-            © 2020 MY PORTFOLIO
-          </p>
-          <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-            <Link
-              target="_blank"
-              href={"https://github.com/hooriyaa"}
-              className="text-gray-500 text-3xl hover:text-[#0a0a0a]"
-            >
-              <ImGithub />
-            </Link>
+            </span>
+      </a>
+      <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 font-serif sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
+      © 2024 MY PORTFOLIO
+      </p>
+      <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+          <Link
+            target="_blank"
+            href={"https://github.com/hooriyaa"}
+            className="text-gray-500 text-3xl hover:text-[#0a0a0a]"
+          >
+            <ImGithub />
+          </Link>
 
-            <Link
-              target="_blank"
-              href={
-                "https://www.linkedin.com/in/hooriya-muhammad-fareed-57a320302/"
-              }
-              className="text-gray-500 text-3xl hover:text-[#0077B5] ml-2"
-            >
-              <ImLinkedin />
-            </Link>
-          </span>
-        </div>
-      </footer>
-    </div>
+          <Link
+            target="_blank"
+            href={
+              "https://www.linkedin.com/in/hooriya-muhammad-fareed-57a320302/"
+            }
+            className="text-gray-500 text-3xl hover:text-[#0077B5] ml-2"
+          >
+            <ImLinkedin />
+          </Link>
+        </span>
+      </div>
+      </div>
+    </footer>
+ 
   );
 };
 
--- src/app/component/navbar.tsx
+++ src/app/component/navbar.tsx
@@ -4,44 +4,43 @@ import { AiOutlineCloudDownload } from "react-icons/ai";
 
 const Navbar = () => {
   return (
-    <div className="bg-white z-50 sticky top-0 font-serif">
-      <header className="text-gray-600 body-font ">
-        <div className="container mx-auto flex flex-wrap p-2 flex-col md:flex-row items-center">
-          <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-            <h1 className="text-3xl mr-7 -ml-6 font-serif font-semibold mb-2">
+    <header className="text-gray-600 body-font">
+      <div className="bg-white z-50 sticky top-0 font-serif">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <span className="ml-3 text-blac text-3xl font-serif font-semibold ">
               Hooriya
-              <span className="  text-3xl text-blue-700 font-serif font-semibold ml-[9px] ">
+              <span className="ml-3  text-3xl font-serif font-semibold text-blue-700">
                 Fareed
               </span>
-            </h1>
+            </span>
           </a>
-
-          <nav className="md:ml-auto md:mr-66 flex flex-wrap items-center text-base justify-center">
-            <Link href={"/"} className="mr-5 hover:text-gray-900">
-              Home
-            </Link>
-            <Link href={"#About"} className="mr-5 hover:text-blue-600">
-              About
-            </Link>
-            <Link href={"#Skills"} className="mr-5 hover:text-blue-600">
-              Skills
-            </Link>
-            <Link href={"#Project"} className="mr-5 hover:text-blue-600">
-              Projects
-            </Link>
-            <Link href={"#Contact"} className="mr-5 hover:text-blue-600">
-              Contact
-            </Link>
-          </nav>
-          <a target="_blank" href="/assests/cv/Profile (4).pdf">
-            <button className=" inline-flex ml-6 items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-              Download CV
-              <AiOutlineCloudDownload className="text-xl ml-2  " />
-            </button>
-          </a>
+          <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+          <Link href={"/"} className="mr-5 hover:text-gray-900">
+            Home
+          </Link>
+          <Link href={"#About"} className="mr-5 hover:text-gray-900">
+            About
+          </Link>
+          <Link href={"#Skills"} className="mr-5 hover:text-gray-900">
+            Skills
+          </Link>
+          <Link href={"#Project"} className="mr-5 hover:text-gray-900">
+            Projects
+          </Link>
+          <Link href={"#Contact"} className="mr-5 hover:text-gray-900">
+            Contact
+          </Link>
+        </nav>
+        <a target="_blank" href="/assests/cv/Profile (4).pdf">
+          <button className=" inline-flex ml-6 items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+            Download CV
+            <AiOutlineCloudDownload className="text-xl ml-2  " />
+          </button>
+        </a>
         </div>
-      </header>
-    </div>
+      </div>
+    </header>
   );
 };
 
