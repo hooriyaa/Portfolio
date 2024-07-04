7f717923ba98108b520e62b37e0d3c0e1a1b859e
Hooriya M. Fareed <parih3080@gmail.com>
2024-07-05 02:19:41 +0500

    push changes

--- src/app/component/navbar.tsx
+++ src/app/component/navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <div className="bg-white z-50 sticky top-0 font-serif">
     <header className="text-gray-600 body-font">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+          <a className="flex title-font font-medium items-center text-gray-900">
             <span className="ml-3 text-blac text-3xl font-serif font-semibold ">
               Hooriya
               <span className="ml-3  text-3xl font-serif font-semibold text-blue-700">
