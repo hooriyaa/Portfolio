b5b7e04f47af1304b867c041032a17778f682687
Hooriya M. Fareed <parih3080@gmail.com>
2024-10-03 17:26:11 +0500

    push_changes

--- src/app/component/project.tsx
+++ src/app/component/project.tsx
@@ -2,8 +2,10 @@
 import Image from "next/image";
 import image1 from "../../../public/assests/projects/facebook_login_page_clone.png";
 import image2 from "../../../public/assests/projects/calculator.png";
-import image3 from "../../../public/assests/projects/amazon_clone.jpg.png";
-import image4 from "../../../public/assests/projects/e-commerce_website.jpg.png";
+import image3 from "../../../public/assests/projects/amazon_clone.png";
+import image4 from "../../../public/assests/projects/e-commerce_website.png";
+import image5 from "../../../public/assests/projects/blog-site.png";
+import image6 from "../../../public/assests/projects/to-do-app.png";
 import Link from "next/link";
 import React from "react";
 
@@ -119,7 +121,7 @@ const Project = () => {
                 </div>
               </div>
             </div>
-            <div className="lg:w-1/2 sm:w-1/2 p-4 cursor-pointer">
+            <div className="lg:w-1/3  sm:w-1/3 p-4 cursor-pointer">
               <div className="flex relative">
                 <Image
                   alt=" E-commerce Website "
@@ -144,6 +146,69 @@ const Project = () => {
                   <Link
                     target="_blank"
                     href={"https://ecommerce-site-by-hooriya.netlify.app/"}
+                  >
+                    <p className="leading-relaxed font-serif text-blue-500 hover:underline">
+                      View Project..
+                    </p>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="lg:w-1/3  sm:w-1/3 p-4 cursor-pointer">
+              <div className="flex relative">
+                <Image
+                  alt=" E-commerce Website "
+                  className="absolute inset-0 h-full w-full object-contain object-center"
+                  src={image5}
+                />
+                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                  <h2 className="tracking-widest text-sm title-font font-serif font-medium text-blue-500 mb-1">
+                    Dynamic Multi-Page Blog Project
+                  </h2>
+                  <h1 className="title-font text-lg font-serif font-medium text-gray-900 mb-3">
+                    Dynamic Multi-Page Blog site
+                  </h1>
+                  <p className="leading-relaxed font-serif line-clamp-3">
+                    I developed a Multi-Page Blog using Next.js and Tailwind
+                    CSS, featuring dynamic routing for seamless navigation and
+                    an interactive comments section to boost user engagement.
+                    This project highlights my skills in creating scalable,
+                    responsive web applications with clean design.
+                  </p>
+                  <Link
+                    target="_blank"
+                    href={"https://dynamic-blog-site-by-hooriya.netlify.app/"}
+                  >
+                    <p className="leading-relaxed font-serif text-blue-500 hover:underline">
+                      View Project..
+                    </p>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="lg:w-1/3  sm:w-1/3 p-4 cursor-pointer">
+              <div className="flex relative">
+                <Image
+                  alt=" E-commerce Website "
+                  className="absolute inset-0 h-full w-full object-contain object-center"
+                  src={image6}
+                />
+                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                  <h2 className="tracking-widest text-sm title-font font-serif font-medium text-blue-500 mb-1">
+                    To-do App Project
+                  </h2>
+                  <h1 className="title-font text-lg font-serif font-medium text-gray-900 mb-3">
+                    To-do App
+                  </h1>
+                  <p className="leading-relaxed font-serif line-clamp-3">
+                    I built a To-Do App featuring add, edit, and delete
+                    functionality for tasks. With optimized performance and a
+                    clean, responsive design, this project showcases my ability
+                    to create efficient, user-friendly applications.
+                  </p>
+                  <Link
+                    target="_blank"
+                    href={"https://to-do-app-by-hooriya.netlify.app/"}
                   >
                     <p className="leading-relaxed font-serif text-blue-500 hover:underline">
                       View Project..
